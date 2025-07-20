8df1cb2b2052341460b1beec0df805b63aa656c5
Ana Letícia Cabral do Rosário <cabraldorosarioanaleticia@gmail.com>
2025-07-19 22:10:58 -0300

    Atividades

---
+++ Atividades Curso em Vídeo/java-basico/estrutura-de-repeticao-maio-2025/Repeticao01/src/main/java/com/mycompany/repeticao01/Repeticao01.java
@@ -0,0 +1,18 @@
+// Código de repetição
+
+package com.mycompany.repeticao01;
+
+
+public class Repeticao01 {
+
+    public static void main(String[] args) {
+        int cc;
+        
+        cc = 1;
+        
+        do{
+           System.out.println("cambalhota " + cc);
+           cc++;
+        } while (cc<=4);
+    }
+}
---
+++ Atividades Curso em Vídeo/java-basico/estrutura-de-repeticao-maio-2025/RepeticaoFor/src/main/java/com/mycompany/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,18 @@
+// Código que mostre repetição com variável de controle
+
+package com.mycompany.repeticaofor;
+
+
+public class RepeticaoFor {
+
+    public static void main(String[] args) {
+      
+        for (int cc = 1; cc<=100; cc += 10){
+            
+            System.out.println("Cambalhota " + cc);
+        
+        
+        
+        }
+    }
+}
